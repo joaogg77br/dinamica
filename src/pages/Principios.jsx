2ea1fdb8a542cd3a5661eb90977bfb8687f8a0ff
joaogg77br <jaogbr77gg@gmail.com>
2024-09-17 14:21:34 +0000

    commitei de novo

--- src/pages/Principios.jsx
+++ src/pages/Principios.jsx
@@ -18,53 +18,53 @@ export default function Principios() {
             </div>
             <section className="bg-zinc-100 p-4 w-full">
                 <div className="flex flex-col text-xl font-medium">
-                    <div className="relative">
+                    <div className="">
                         <div className="flex justify-center p-2 bg-orange-500 rounded-t-2xl w-16  text-white">1</div>
                         <div className="flex rounded-tr-2xl rounded-bl-2xl bg-white">
-                            <img src={Adesao} alt="" className="w-3/4" />
+                            <img src={Adesao} alt="" className="w-2/4 p-4" />
                             <p className="text-2xl p-8">Adesão Voluntária e Livre</p>
                         </div>
                     </div>
                     <div className="flex justify-end flex-col items-end">
                         <div className="flex justify-center p-4 bg-orange-500 w-16 text-white font-medium">2</div>
                         <div className="flex w-full rounded-br-2xl rounded-tl-2xl p-4  bg-white">
-                            <img src={Gestao} alt="" className="w-3/4" />
-                            <p className="text-2xl p-8">Gestão Democrática e Livre</p>
+                            <img src={Gestao} alt="" className="w-2/4" />
+                            <p className="text-2xl p-8 w-2/4">Gestão Democrática e Livre</p>
                         </div>
                     </div>
                     <div className="flex justify-end flex-col items-start">
                         <div className="flex justify-center p-4 bg-orange-500 w-16 text-white">3</div>
                         <div className="flex text-2xl w-full rounded-tr-2xl rounded-bl-2xl p-4 bg-white">
-                            <img src={Pizza} alt="" className="w-3/4"/>
-                            <p className="text-2xl p-8">Participação Econômica dos Cooperados</p>
+                            <img src={Pizza} alt="" className="w-2/4"/>
+                            <p className="text-2xl p-8 w-2/4">Participação Econômica dos Cooperados</p>
                         </div>
                     </div>
                     <div className="flex justify-end flex-col items-end">
                         <div className="flex justify-center p-4 bg-orange-500 w-16 text-white">4</div>
                         <div className="flex  text-2xl w-full rounded-tr-2xl rounded-bl-2xl p-4 bg-white">
-                            <img src={Card} alt="" className="w-3/4"/>
-                            <p className="text-2xl p-8">Autonomia e Independência</p>
+                            <img src={Card} alt="" className="w-2/4"/>
+                            <p className="text-2xl p-8 w-2/4">Autonomia e Independência</p>
                         </div>
                     </div>
                     <div className="flex justify-end flex-col items-start">
                         <div className="flex justify-center p-4 bg-orange-500 w-16 text-white">5</div>
                         <div className="flex w-full rounded-tr-2xl rounded-bl-2xl p-4 bg-white">
-                            <img src={Education} alt=""  className="w-3/4"/>
-                            <p className="text-2xl p-8">Educação, Formação e Informação</p>
+                            <img src={Education} alt=""  className="w-2/4"/>
+                            <p className="text-2xl p-8 w-2/4">Educação, Formação e Informação</p>
                         </div>
                     </div>
                     <div className="flex justify-end flex-col items-end">
                         <div className="flex justify-center p-4 bg-orange-500 w-16 text-white">6</div>
                         <div className="flex w-full rounded-tr-2xl rounded-bl-2xl p-4 bg-white">
-                            <img src={People} alt=""  className="3/4"/>
-                            <p className="text-2xl p-8">Intercoo-peração</p>
+                            <img src={People} alt=""  className="2/4"/>
+                            <p className="text-2xl p-8 w-2/4">Intercoo-peração</p>
                         </div>
                     </div>
                     <div className="flex justify-end flex-col items-start">
                         <div className="flex justify-center p-4 bg-orange-500 w-16 text-white">7</div>
                         <div className="flex w-full rounded-tr-2xl rounded-bl-2xl p-4 bg-white">
-                            <img src={Community} alt="" className="w-3/4"/>
-                            <p className="text-2xl p-8">Interesse Pela Comunidade</p>
+                            <img src={Community} alt="" className="w-2/4"/>
+                            <p className="text-2xl p-8 w-2/4">Interesse Pela Comunidade</p>
                         </div>
                     </div>
                 </div>
