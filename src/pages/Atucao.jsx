3140f9019307e3d8853e0ae3386298ca6298e7f6
joaogg77br <jaobgr77gg@gmail.com>
2024-09-19 17:34:34 -0300

    por favor

--- src/pages/Atucao.jsx
+++ src/pages/Atucao.jsx
@@ -1,7 +1,7 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Info from "../components/Info";
-import manual from "../assets/map.svg"
+import manual from "../assets/brazil.svg"
 
 export default function Atuacao() {
     return (
