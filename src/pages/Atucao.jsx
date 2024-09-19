765a3ca20073e46c6d9b204309a1292d21add2fe
joaogg77br <jaobgr77gg@gmail.com>
2024-09-19 17:04:27 -0300

    alterações

--- src/pages/Atucao.jsx
+++ src/pages/Atucao.jsx
@@ -1,7 +1,6 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Info from "../components/Info";
-import Map from "../assets/map.svg"
 
 export default function Atuacao() {
     return (
@@ -12,7 +11,6 @@ export default function Atuacao() {
             </div>
                 <div className="flex flex-col flex-wrap-reverse w-full gap-4 p-16 bg-zinc-100">
                     <p className=" text-center lg:text-md">A Dinâmica possui mais <span className="font-bold">de 6 mil cooperados</span>, e presta seus serviços através de seus cooperados em empresas públicas e privadas, <br></br>presentes nos estados de Alagoas, Pernambuco e Paraíba.</p>
-                    <img className="lg:h-96 lg:96" src={Map} alt="" />
                 </div>
                 <Footer/>
         </>
