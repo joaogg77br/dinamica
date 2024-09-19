9e4de6f20a02f6cc21c1d7f4c45ee476dc1c9d46
joaogg77br <jaobgr77gg@gmail.com>
2024-09-19 17:17:31 -0300

    vendo se dessa vez vai o deploy

--- src/components/Advantages.jsx
+++ src/components/Advantages.jsx
@@ -3,18 +3,18 @@
 export default function Advantagens({text,img,number}){
     return(
         <div className="">
-            <div className="relative h-40 w-40 md:h-52 md:w-52 bg-laranja rounded-2xl 
+            <div className="relative h-60 w-40 md:h-52 md:w-52 bg-laranja rounded-2xl 
             p-10
             sm:h-40 sm:w-60 sm:p-0
             ">
                 <div className="flex h-10 w-10 justify-center items-center absolute top-fifityMin left-fifityMin bg-verde text-white rounded-xl 
                     sm:h-12 sm:w-12
                 ">{number}</div>
-                <div className="flex flex-col justify-center text-white w-full h-40  text-center p-2 lg:pt-10 ">
-                    <div className="h-20 flex items-center justify-center">
+                <div className="flex flex-col justify-center text-white w-full text-center  lg:pt-10 ">
+                    <div className="flex flex-col items-center justify-center">
                     <img src={img} alt="" />
+                    <p className="">{text}</p>
                     </div>
-                    <p>{text}</p>
                 </div>
             </div>
         </div>
--- src/pages/Atucao.jsx
+++ src/pages/Atucao.jsx
@@ -1,6 +1,7 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Info from "../components/Info";
+import Mapa from "../assets/map.svg"
 
 export default function Atuacao() {
     return (
@@ -11,6 +12,7 @@ export default function Atuacao() {
             </div>
                 <div className="flex flex-col flex-wrap-reverse w-full gap-4 p-16 bg-zinc-100">
                     <p className=" text-center lg:text-md">A Dinâmica possui mais <span className="font-bold">de 6 mil cooperados</span>, e presta seus serviços através de seus cooperados em empresas públicas e privadas, <br></br>presentes nos estados de Alagoas, Pernambuco e Paraíba.</p>
+                    <img className="lg:h-96 lg:96" src={Mapa} alt="" />
                 </div>
                 <Footer/>
         </>
