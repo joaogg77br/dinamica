94ec9eb427cf61500f8273535d20fbbcf78424f0
joaogg77br <jaobgr77gg@gmail.com>
2024-10-16 16:54:35 -0300

    ok

--- src/pages/PagecontainerA.jsx
+++ src/pages/PagecontainerA.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom"
 import { X } from "lucide-react"
 import Dinamica from "../assets/Pdfs/Dinâmica News - 2ª edição.pdf"
-
+import PdfObject from "pdfobject"
+import { useEffect } from "react"
 export default function PagecontainerA() {
+    useEffect(()=>{
+        PdfObject.embed(Dinamica,"#pdf-viewer");
+    },[])
     return (
            
                 <div className="w-full h-screen text-white top-2 left-0 z-50 ">
                     <Link to="/conteudos" className="text-white">
                     <button className="position rounded absolute bg-laranja p-2"><X/></button>
                     </Link>
-                    <embed src={Dinamica} type="application/pdf" className="w-full h-screen" />
+                    <div id="pdf-viewer" style={{ width: '100%', height: '800px' }}></div>
                 </div>
         )
     }
